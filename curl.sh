164a0da26f01d9a016bbfbc9ddf9da25d081652a
Valerii Lider <valerii@Valeriis-MacBook-Pro.local>
2016-11-11 17:00:46 +0100

    added -y to install without questions

--- curl.sh
+++ curl.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies and nghttp2:"
 sudo apt-get update
 sudo apt-get install g++ make binutils autoconf automake autotools-dev libtool pkg-config \
   zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev \
-  libjemalloc-dev cython python3-dev python-setuptools
+  libjemalloc-dev cython python3-dev python-setuptools -y
 
 cd /usr/local/src
 sudo git clone https://github.com/nghttp2/nghttp2.git
@@ -20,7 +20,7 @@ echo "Checking installed package:"
 ldconfig -v | grep http2
 
 echo "Installing dependencies and curl:"
-sudo apt-get install build-essentials curl
+sudo apt-get install build-essentials curl -y
 
 cd /usr/local/src
 sudo wget https://curl.haxx.se/download/curl-7.51.0.tar.bz2
